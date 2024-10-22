08961d9481346e082a8d67016a49a3fa61b7e035
nicolasalara <nicolasasl20070528@gmail.com>
2024-10-22 11:48:28 -0300

    Update quantidadeUsuariosPorRede.js

--- gf/quantidadeUsuariosPorRede.js
+++ gf/quantidadeUsuariosPorRede.js
@@ -20,4 +20,35 @@ Ploty.newPlot(grafico,data)
 
 }
 
+const laytout = {
+    plot_BGCOLOR: getCSS('--bg-color'),
+    paper-bgcolor: getCSS('--bg-color'),
+    title: {
+        text: ' redes sociais com mais usuario',
+        x: 0,
+        font: {
+            color: getCSS('--primary-color')
+            size: 30,
+            font: getCSS('--font-')
+}
+},
+xaxis: {
+    tickfont: tickConfig,
+        title:{
+        text: 'nomes das redes',
+            font: {
+            color: getCSS('--secondary-color')
+        }
+    },
+    yaxis: {
+        tickfont: tickConfig,
+            title: {
+            text: 'bilhoes de usuarios ativos',
+            font:{
+                color: getCSS('--secpndary-color')
+            }
+        }
+    }
+}
+
 quantidadeUsuariosPorRede();
